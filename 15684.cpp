95155b1324cfeb5ad71e729a552452ba60f76af7
ljt0815 <ljt0815@naver.com>
2022-05-25 12:08:39 +0000

    15684 sadari... but difficult

--- 15684.cpp
+++ 15684.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int N, M, H;
-bool a[11][31];
-
+bool a[31][11];
+long long result = 2147483647;
 bool    valid(void)
 {
     for (int X = 1; X <= N; X++) {
         int ch = X;
         for (int Y = 1; Y <= H; Y++) {
-            if (a[Y][ch] == 1) {
+            if (a[Y][ch] == true) {
                 ch++;
             }
-            else if (a[Y][ch - 1] == 1) {
+            else if (a[Y][ch - 1] == true) {
                 ch--;
             }
         }
@@ -24,27 +24,42 @@ bool    valid(void)
     return true;
 }
 
-void    dfs(int depth)
+void    dfs(int depth, int y, int x)
 {
-    if (!valid()) return ;
-    if (depth == N)
+    if (depth >= result)    return ;
+    if (valid())
     {
-
+        result = depth;
+        return ;
     }
-    int i = depth;
-    for (int j = 1; j <= H; j++)
+    if (depth == 3) return;
+    for (int i = y; i <= H; i++)
     {
-        if (a[i][j]) continue;
-        if (a[i - 1][j]) continue;
-        if (a[i + 1][j]) continue;
-        a[i][j] == true;
-        dfs(depth + 1);
-        a[i][j] == false;
+        for (int j = x; j < N; j++)
+        {
+            if (a[i][j]) continue;
+            if (a[i][j - 1]) continue;
+            if (a[i][j + 1]) continue;
+            a[i][j] = true;
+            dfs(depth + 1, i, j);
+            a[i][j] = false;
+        }
+        y = 1;
     }
 }
 
 int main(void)
 {
+    int x, y;
     cin >> N >> M >> H;
+    for (int i = 0; i < M; i++)
+    {
+        cin >> x >> y;
+        a[x][y] = true;
+    }
+    dfs(0, 1, 1);
+    if (result == 2147483647)
+        result = -1;
+    cout << result << "\n";
     return 0;
 }
